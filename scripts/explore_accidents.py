a6f4e91bae9e4c3dc615492e8de30143e6ef4906
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-05-12 17:43:23 +0200

    Create day counter though dataset

--- scripts/explore_accidents.py
+++ scripts/explore_accidents.py
@@ -7,15 +7,18 @@ Created on Fri May 12 15:02:10 2023
 """
 
 import pandas as pd
+import numpy as np
+import datetime
+
 
 # Enlarge output display capacity
 pd.set_option('display.max_rows', 500)
 
 # Import Zuerich city accidents dataset
-accidents = pd.read_csv('data/RoadTrafficAccidentLocations.csv', header = 0)
+accidents = pd.read_csv('../data/RoadTrafficAccidentLocations.csv', header = 0)
 
 # View an instance
-accidents.loc[3, :]
+accidents.loc[34649, :]
 
 # Drop duplicate columns, only keep English ones
 accidents = accidents.drop(columns = ['AccidentType_de', 'AccidentType_fr', 'AccidentType_it', 
@@ -43,3 +46,21 @@ accidents[['AccidentYear', 'AccidentMonth_en','AccidentWeekDay', 'AccidentWeekDa
 # Only keep data in 2019
 
 accidents = accidents[accidents.AccidentYear == 2019]
+
+#### Create date fixing loop
+# Create new column for date
+accidents["DayCount"] = np.nan
+
+# Reset indices             
+accidents = accidents.reset_index(drop=True)           
+
+# Initialise counter
+count = 1    
+
+# Loop though data and add day counter
+for row in accidents.index:
+    accidents.at[row, 'DayCount'] = count   #Set first day of the month to 1
+    if (accidents.at[row, 'AccidentWeekDay'] != accidents.at[row+1, 'AccidentWeekDay']): # If the days change
+        count = count + 1 # Add to the counter (change the dates)
+        accidents.at[row+1, 'DayCount'] = count  #Add counter as day counter
+         
